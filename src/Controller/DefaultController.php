792e595e604530275bda3443d787e3ca11a3044c
KostenkoA <kostenkoam1212@gmail.com>
2018-06-26 12:09:58 +0300

    MVC first route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Model\ContactsPage;
+use App\Service\MessageGenerator;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    public function index()
+    private $messageGeneraror;
+    private $logger;
+
+    public function __construct(MessageGenerator $messageGeneraror, LoggerInterface $logger)
+    {
+        $this->messageGeneraror = $messageGeneraror;
+        $this->logger = $logger;
+    }
+
+    public function index(): Response
     {
         return new Response('Symfony4 is great');
     }
 
-    public function contacts()
+    public function contacts(): Response
     {
-        return $this->render('default/index.html.twig');
+        $this->logger->info('User reached contacts page');
+
+        $model = new ContactsPage('info@itea.ua', $this->messageGeneraror->generate());
+
+        return $this->render('default/index.html.twig',['contacts' => $model]);
     }
 }
--- src/Model/ContactsPage.php
+++ src/Model/ContactsPage.php
@@ -2,8 +2,24 @@
 
 namespace App\Model;
 
-
 class ContactsPage
 {
+    private $email;
+    private $quote;
 
+    public function __construct(string $email, string $quote)
+    {
+        $this->email = $email;
+        $this->quote = $quote;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getQuote(): string
+    {
+        return $this->quote;
+    }
 }
