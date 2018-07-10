e76b5a08dec6e38f1b0ea8530516137841477f54
KostenkoA <kostenkoam1212@gmail.com>
2018-07-10 11:58:26 +0300

    Added PHPdoc

--- src/Model/ContactsPage.php
+++ src/Model/ContactsPage.php
@@ -2,22 +2,50 @@
 
 namespace App\Model;
 
+/**
+ * Model for contacts page
+ *
+ * @author Kostenko Anton <kostenko.antony@gmail.com>
+ */
 class ContactsPage
 {
+    /**
+     * @var string
+     */
     private $email;
+    /**
+     * @var string
+     */
     private $quote;
 
+    /**
+     * ContactsPage constructor.
+     * @param string $email
+     * @param string $quote
+     *
+     * @throws \RuntimeException If email is empty
+     */
     public function __construct(string $email, string $quote)
     {
+        if (empty($email)){
+            throw new \RuntimeException('Email cannot be empty');
+        }
+
         $this->email = $email;
         $this->quote = $quote;
     }
 
+    /**
+     * @return string
+     */
     public function getEmail(): string
     {
         return $this->email;
     }
 
+    /**
+     * @return string
+     */
     public function getQuote(): string
     {
         return $this->quote;
