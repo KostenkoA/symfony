f1086d4532907ae378891040dd4aeffea67b3b8b
KostenkoA <kostenkoam1212@gmail.com>
2018-07-10 12:11:48 +0300

    Created 2 tetst

---
+++ tests/Controller/DefautlControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefautlControllerTest extends WebTestCase
+{
+    public function testContacts()
+    {
+        $client = self::createClient();
+
+        $crawler = $client->request('GET', '/contacts');
+
+        self::assertEquals(200, $client->getResponse()->getStatusCode());
+
+        self::assertEquals(1, $crawler->filter('a:contains("info@itea.ua")')->count());
+    }
+
+}
